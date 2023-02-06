6001784603ece7c16a1f863c047e31c01e703387
suga-ming <kby9901@naver.com>
2023-02-06 21:55:45 +0900

    Feat(comunity):좋아요

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
-import { postInfoInterface } from "../api/comunity";
+import {
+  checkLikedInterface,
+  postInfoInterface,
+  postLiked,
+} from "../api/comunity";
+import { isAccessToken } from "../store/recoil";
 
 const PostArea = styled.div`
   /* border: 1px solid gray; */
@@ -53,10 +60,34 @@ const Board = ({
   commentCount,
 }: postInfoInterface) => {
   const navigate = useNavigate();
+  const accessToken = useRecoilValue(isAccessToken);
+  const [like, setLike] = useState(liked);
 
   const goPost = () => {
     navigate(`/comunity/${boardId}`);
   };
+
+  // const { isLoading, data } = useQuery<checkLikedInterface>([`info`], () =>
+  //   postLiked(boardId, accessToken)
+  // );
+  // useEffect(() => {
+  //   // ! 화면이 맨 처음 렌더링 될때 안에 있
+  //   postLiked(boardId, accessToken).then((res) => {
+  //     res?.data
+
+  //   });
+
+  // }, [like]);
+
+  // const changeLiked = async () => {
+  //   // ! 좋아요 누르는 api 선언
+  //   const res = await postLiked(boardId, accessToken);
+  //   setLike(res.data.liked);
+  //   console.log(like);
+  // };
+
+  // const list = data?.data
+  // console.log();
   return (
     <PostArea>
       <div className="px-7 cursor-pointer" onClick={goPost}>
@@ -99,7 +130,7 @@ const Board = ({
       <Solid className="flex justify-around">
         <Solid2
           className="flex py-3 w-1/2 justify-center cursor-pointer"
-          onClick={() => !liked}
+          onClick={changeLiked}
         >
           <svg
             className="w-4 mr-2"
@@ -107,11 +138,11 @@ const Board = ({
             viewBox="0 0 512 512"
           >
             <Path
-              like={liked}
+              like={like}
               d="M226.5 92.9c14.3 42.9-.3 86.2-32.6 96.8s-70.1-15.6-84.4-58.5s.3-86.2 32.6-96.8s70.1 15.6 84.4 58.5zM100.4 198.6c18.9 32.4 14.3 70.1-10.2 84.1s-59.7-.9-78.5-33.3S-2.7 179.3 21.8 165.3s59.7 .9 78.5 33.3zM69.2 401.2C121.6 259.9 214.7 224 256 224s134.4 35.9 186.8 177.2c3.6 9.7 5.2 20.1 5.2 30.5v1.6c0 25.8-20.9 46.7-46.7 46.7c-11.5 0-22.9-1.4-34-4.2l-88-22c-15.3-3.8-31.3-3.8-46.6 0l-88 22c-11.1 2.8-22.5 4.2-34 4.2C84.9 480 64 459.1 64 433.3v-1.6c0-10.4 1.6-20.8 5.2-30.5zM421.8 282.7c-24.5-14-29.1-51.7-10.2-84.1s54-47.3 78.5-33.3s29.1 51.7 10.2 84.1s-54 47.3-78.5 33.3zM310.1 189.7c-32.3-10.6-46.9-53.9-32.6-96.8s52.1-69.1 84.4-58.5s46.9 53.9 32.6 96.8s-52.1 69.1-84.4 58.5z"
             />
           </svg>
-          <Text like={liked} className="text-base">
+          <Text like={like} className="text-base">
             공감해요
           </Text>
         </Solid2>
