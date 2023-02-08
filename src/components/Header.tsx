2a92ca058e2aa6ac0806233f0ff9977af1c0a3c3
suga-ming <kby9901@naver.com>
2023-02-08 16:55:27 +0900

    Design(Header):로그인 했을 때

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import { useRecoilState, useRecoilValue, useResetRecoilState } from "recoil";
 import { isAccessToken, isLogin, isRefreshToken } from "../store/recoil";
+import dane from "../assets/단.png";
+import { useState } from "react";
+import styled from "styled-components";
+
+const Solid = styled.div`
+  border-bottom: 1px solid rgb(156 163 175);
+`;
+
+const Solid2 = styled.div`
+  border: 1px solid rgb(156 163 175);
+`;
 
 const Header = () => {
   const navigate = useNavigate();
   const [login, setLogin] = useRecoilState(isLogin);
+  const [modal, setModal] = useState(false);
   const accessToken = useRecoilValue(isAccessToken);
   const isLoginReset = useResetRecoilState(isLogin);
   const isAccessTokenReset = useResetRecoilState(isAccessToken);
@@ -12,68 +24,109 @@ const Header = () => {
 
   const goLogin = () => {
     navigate("/login");
+    setModal(false);
   };
   const goSignUp = () => {
     navigate("/signUp");
+    setModal(false);
   };
   const goHome = () => {
     navigate("/home");
+    setModal(false);
   };
   const goMyPage = () => {
     navigate("/myPage");
+    setModal(false);
   };
   const goLogOut = () => {
+    setModal(false);
     isLoginReset();
     isAccessTokenReset();
     isRefreshTokenReset();
     navigate("/home");
   };
+  const goMyProfile = () => {
+    navigate("/myProfile");
+    setModal(false);
+  };
+
+  console.log(modal);
 
   return (
-    <div className="flex w-full justify-between px-10 py-5 bg-pet_pink fixed z-50">
-      <div onClick={goHome} className="font-bold cursor-pointer">
-        ForDog
-      </div>
-      {/* <div className="flex font-semibold">
-        <div className="px-5">자랑하기</div>
-        <div className="px-5">커뮤니티</div>
-        <div className="px-5">캘린더</div>
-        <div className="px-5">랭킹</div>
-      </div> */}
-      <div className="flex">
-        <svg
-          className="w-5 mr-4"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
-        >
-          <path
-            fill="white"
-            d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM352 256c0 53-43 96-96 96s-96-43-96-96s43-96 96-96s96 43 96 96zm32 0c0-70.7-57.3-128-128-128s-128 57.3-128 128s57.3 128 128 128s128-57.3 128-128z"
-          />
-        </svg>
-        {login ? (
-          <div className="flex items-center font-medium">
-            <div onClick={goMyPage} className="cursor-pointer">
-              마이페이지
-            </div>
-            <div className="w-px h-3 mx-2 bg-black" />
-            <div onClick={goLogOut} className="cursor-pointer">
-              로그아웃
+    <>
+      {login ? (
+        <div className="flex w-full justify-between items-center px-10 py-3 bg-pet_pink fixed z-50">
+          <div onClick={goHome} className="font-bold cursor-pointer">
+            ForDog
+          </div>
+          {/* <div className="flex font-semibold">
+            <div className="px-5">자랑하기</div>
+            <div className="px-5">커뮤니티</div>
+            <div className="px-5">캘린더</div>
+            <div className="px-5">랭킹</div>
+          </div> */}
+          <div className="flex">
+            <div className="relative flex items-center font-medium">
+              <div
+                onClick={() => setModal(!modal)}
+                className="flex mr-5 cursor-pointer text-lg"
+              >
+                <div className="mr-1 font-semibold">보영쨩</div>
+                <div className="mr-1 font-medium">님</div>
+                <svg
+                  className="w-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path d="M201.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 338.7 54.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+                </svg>
+              </div>
+              <img
+                src={dane}
+                onClick={goMyProfile}
+                className="w-10 rounded-full cursor-pointer"
+              />
+              {modal ? (
+                <Solid2 className="absolute top-10 left-0 text-sm bg-white text-center rounded-lg">
+                  <Solid
+                    onClick={goMyPage}
+                    className="py-3 px-3 cursor-pointer"
+                  >
+                    마이페이지
+                  </Solid>
+                  <div onClick={goLogOut} className="py-3 px-3 cursor-pointer">
+                    로그아웃
+                  </div>
+                </Solid2>
+              ) : null}
             </div>
           </div>
-        ) : (
-          <div className="flex items-center font-medium">
-            <div onClick={goLogin} className="cursor-pointer">
-              로그인
-            </div>
-            <div className="w-px h-3 mx-2 bg-black" />
-            <div onClick={goSignUp} className="cursor-pointer">
-              회원가입
+        </div>
+      ) : (
+        <div className="flex w-full justify-between items-center px-10 py-6 bg-pet_pink fixed z-50">
+          <div onClick={goHome} className="font-bold cursor-pointer">
+            ForDog
+          </div>
+          {/* <div className="flex font-semibold">
+            <div className="px-5">자랑하기</div>
+            <div className="px-5">커뮤니티</div>
+            <div className="px-5">캘린더</div>
+            <div className="px-5">랭킹</div>
+          </div> */}
+          <div className="flex">
+            <div className="flex items-center font-medium">
+              <div onClick={goLogin} className="cursor-pointer">
+                로그인
+              </div>
+              <div className="w-px h-3 mx-2 bg-black" />
+              <div onClick={goSignUp} className="cursor-pointer">
+                회원가입
+              </div>
             </div>
           </div>
-        )}
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
