c7385b134bb7268e4aaf7bb377dc7ed157d993ef
suga-ming <kby9901@naver.com>
2023-03-02 16:37:05 +0900

    Fix(Calendar):css 위치

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,4 +1,4 @@
-import "./Calendar.css";
+import "../assets/css/Calendar.css";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import allLocales from "@fullcalendar/core/locales-all";
